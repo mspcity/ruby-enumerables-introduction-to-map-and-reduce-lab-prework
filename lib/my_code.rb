ffaca5001d089eb951aead515a8bf4ac674cebbf
Youngmin Park <codepark2019@gmail.com>
2020-05-07 07:14:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,12 +52,12 @@ end
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
-    if source_array[i]
-       return true
+    if !source_array[i]
+       return false
     end
       i += 1
     end
-  false
+  true
 end
 
 def reduce_to_any_true(source_array)
